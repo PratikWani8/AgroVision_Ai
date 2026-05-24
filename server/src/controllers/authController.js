e74c04089c6ea75e5ecdcbe9d6ac207c82629f8b
Pratik Wani <pwani8306@gmail.com>
2026-05-24 15:56:16 +0530

    feat: Added authController for user authentication and authorization workflows

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,86 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role
+    });
+
+    res.status(201).json({
+      success: true,
+      token: generateToken(user._id),
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email"
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: generateToken(user._id),
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+
+    res.status(200).json({
+      success: true,
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
