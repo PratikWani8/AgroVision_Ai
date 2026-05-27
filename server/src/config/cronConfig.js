5b5dfe9b421c25c50cf39dcf3d1a93647268f0d5
Pratik Wani <pwani8306@gmail.com>
2026-05-27 14:30:42 +0530

    feat: Added cronConfig for scheduled background job management

---
+++ server/src/config/cronConfig.js
@@ -0,0 +1,16 @@
+export const CRON_SCHEDULES = {
+  WEATHER_SYNC:
+    "0 */6 * * *",
+
+  PREDICTION:
+    "0 */12 * * *",
+
+  ALERT_MONITOR:
+    "*/30 * * * *",
+
+  SATELLITE_PROCESS:
+    "0 0 * * *",
+
+  CLEANUP:
+    "0 2 * * 0"
+};
