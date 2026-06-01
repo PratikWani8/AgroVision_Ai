762b30bd4e4949069afa799d9ff90b8572fd38e4
Pratik Wani <pwani8306@gmail.com>
2026-06-01 13:13:40 +0530

    feat: Add application constants

---
+++ ml-service/app/core/constants.py
@@ -0,0 +1,23 @@
+SATELLITE_PROVIDERS = [
+    "sentinel",
+    "landsat",
+    "earth_engine",
+    "nasa"
+]
+
+RISK_LEVELS = [
+    "LOW",
+    "MEDIUM",
+    "HIGH"
+]
+
+VEGETATION_CLASSES = [
+    "POOR",
+    "MODERATE",
+    "HEALTHY"
+]
+
+MODEL_TYPES = [
+    "xgboost",
+    "random_forest"
+]
