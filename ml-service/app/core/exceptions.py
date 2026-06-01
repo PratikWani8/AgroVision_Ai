39fffcad6a44b31ff481db2c66c3715c0238b65b
Pratik Wani <pwani8306@gmail.com>
2026-06-01 13:12:12 +0530

    feat: Add custom exception handling framework

---
+++ ml-service/app/core/exceptions.py
@@ -0,0 +1,24 @@
+class SatelliteProcessingError(
+    Exception
+):
+    pass
+
+class ModelTrainingError(
+    Exception
+):
+    pass
+
+class PredictionError(
+    Exception
+):
+    pass
+
+class RasterLoadError(
+    Exception
+):
+    pass
+
+class GeoSpatialError(
+    Exception
+):
+    pass
