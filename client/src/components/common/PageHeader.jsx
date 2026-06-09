7a3a383f732e58e68821448e23d9b102c26dcf1e
Pratik Wani <pwani8306@gmail.com>
2026-06-09 12:48:15 +0530

    feat: Add PageHeader component

---
+++ client/src/components/common/PageHeader.jsx
@@ -0,0 +1,44 @@
+function PageHeader({
+
+  title,
+
+  subtitle
+}) {
+
+  return (
+
+    <div
+      className="
+        mb-8
+      "
+    >
+
+      <h1
+        className="
+          text-4xl
+          font-bold
+          text-white
+        "
+      >
+        {title}
+      </h1>
+
+      {
+        subtitle && (
+
+          <p
+            className="
+              text-slate-400
+              mt-2
+            "
+          >
+            {subtitle}
+          </p>
+        )
+      }
+
+    </div>
+  );
+}
+
+export default PageHeader;
